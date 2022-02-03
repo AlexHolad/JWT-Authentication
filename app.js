708d1cd090b17215b5df9c922a5df710856598d2
Alexej Holad <alexej.holad@gmail.com>
2022-02-03 16:27:59 +0100

    cnm

--- app.js
+++ app.js
@@ -36,6 +36,13 @@ app.post("/welcome", auth, (req, res) => {
  });
 
 
+ app.get("*", (req, res) => {
+   let url = path.join(__dirname, '../client/build', 'index.html');
+   if (!url.startsWith('/app/')) // since we're on local windows
+     url = url.substring(1);
+   res.sendFile(url);
+ });
+
 // APP LISTENER
 app.listen(PORT, () => {
   console.log(`Connected to ${PORT}`);
